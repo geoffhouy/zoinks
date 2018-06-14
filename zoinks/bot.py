a79181d3bd83b0dc2beb2ee311e91306417d2a74
Geoffrey Houy <geoffhouy@gmail.com>
2018-06-14 15:05:13 -0400

    Override on_message to ignore all bot messages

--- zoinks/bot.py
+++ zoinks/bot.py
@@ -15,3 +15,8 @@ class ZOINKS(commands.Bot):
     async def on_ready(self):
         await self.change_presence(game=discord.Game(name=f'ZOINKS! | {command_prefix}help'))
         print(f'{self.user} ({self.user.id}) has logged in')
+
+    async def on_message(self, message):
+        if message.author.bot:
+            pass
+        await self.process_commands(message)
