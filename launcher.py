df129c8876a7aed149908bce6e31fbcbe23951a1
Geoffrey Houy <geoffhouy@gmail.com>
2018-06-15 22:29:26 -0400

    Implement basic logging

--- launcher.py
+++ launcher.py
@@ -1,7 +1,16 @@
 import config
 from zoinks.bot import ZOINKS
 
+import logging
+import sys
+
 
 if __name__ == '__main__':
+    logging.getLogger('discord').setLevel(logging.WARNING)
+    logging.basicConfig(
+        format='%(asctime)s %(levelname)s %(name)s: %(message)s',
+        datefmt="%Y-%m-%d %H:%M:%S",
+        level=logging.INFO,
+        stream=sys.stdout)
     bot = ZOINKS()
     bot.run(config.DISCORD_TOKEN)
