6870c19fb9ee6cd061656b46ae54045e0a66ceff
Geoffrey Houy <geoffhouy@gmail.com>
2018-06-18 09:06:05 -0400

    Add webhook to find patch notes

--- zoinks/cogs/realm_royale.py
+++ zoinks/cogs/realm_royale.py
@@ -1,3 +1,5 @@
+from zoinks.webhook import Webhook
+
 import logging
 
 
@@ -8,6 +10,17 @@ class RealmRoyale:
 
     def __init__(self, bot):
         self.bot = bot
+        self.webhook = Webhook(
+            endpoint='457588791062822912/CY8BuF3M8r944g-y4-3svTTI-GOEc9LIACCMnrWkaz-tJAwBURuFpabGUzusUzdsT2Fi',
+            source='https://store.steampowered.com/news/?appids=813820',
+            navigate_html=lambda soup: soup.find(
+                'div', {'class': 'newsPostBlock steam_community_announcements'}).find('a').get('href'),
+            poll_delay=900,
+            title='Realm Royale',
+            icon_url='https://steamcdn-a.akamaihd.net/steam/apps/813820/capsule_184x69.jpg',
+            color=0x9D69F4
+        )
+        self.bot.loop.create_task(self.webhook.poll())
         logger.info(f'{self.__class__.__name__} loaded')
 
 
