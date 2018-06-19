a907ec91286ef2a4917240a568c21b42f5856218
Geoffrey Houy <geoffhouy@gmail.com>
2018-06-19 09:33:44 -0400

    Update to implement webhook changes

--- zoinks/cogs/realm_royale.py
+++ zoinks/cogs/realm_royale.py
@@ -1,4 +1,4 @@
-from zoinks.webhook import Webhook
+from zoinks.webhook import ScrapingWebhook
 
 import logging
 
@@ -10,7 +10,7 @@ class RealmRoyale:
 
     def __init__(self, bot):
         self.bot = bot
-        self.webhook = Webhook(
+        self.steam_webhook = ScrapingWebhook(
             endpoint='457588791062822912/CY8BuF3M8r944g-y4-3svTTI-GOEc9LIACCMnrWkaz-tJAwBURuFpabGUzusUzdsT2Fi',
             source='https://store.steampowered.com/news/?appids=813820',
             navigate_html=lambda soup: soup.find(
@@ -20,7 +20,7 @@ class RealmRoyale:
             icon_url='https://steamcdn-a.akamaihd.net/steam/apps/813820/capsule_184x69.jpg',
             color=0x9D69F4
         )
-        self.bot.loop.create_task(self.webhook.poll())
+        self.bot.loop.create_task(self.steam_webhook.poll())
         logger.info(f'{self.__class__.__name__} loaded')
 
 
