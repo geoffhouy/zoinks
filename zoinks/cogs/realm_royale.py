8f21998b932e557da0af8c000e211eed02095b3a
Geoffrey Houy <geoffhouy@gmail.com>
2018-06-19 09:45:58 -0400

    Change navigate_html from dict arg to keyword arg

--- zoinks/cogs/realm_royale.py
+++ zoinks/cogs/realm_royale.py
@@ -14,12 +14,11 @@ class RealmRoyale:
             endpoint='457588791062822912/CY8BuF3M8r944g-y4-3svTTI-GOEc9LIACCMnrWkaz-tJAwBURuFpabGUzusUzdsT2Fi',
             source='https://store.steampowered.com/news/?appids=813820',
             navigate_html=lambda soup: soup.find(
-                'div', {'class': 'newsPostBlock steam_community_announcements'}).find('a').get('href'),
+                'div', _class='newsPostBlock steam_community_announcements').find('a').get('href'),
             poll_delay=900,
             title='Realm Royale',
             icon_url='https://steamcdn-a.akamaihd.net/steam/apps/813820/capsule_184x69.jpg',
-            color=0x9D69F4
-        )
+            color=0x9D69F4)
         self.bot.loop.create_task(self.steam_webhook.poll())
         logger.info(f'{self.__class__.__name__} loaded')
 
