ff02ecd214c6e236c1263be1ff79792200224649
Geoffrey Houy <geoffhouy@gmail.com>
2018-06-27 15:40:52 -0400

    Add Dota2 webhook

--- zoinks/cogs/webhooks.py
+++ zoinks/cogs/webhooks.py
@@ -22,6 +22,20 @@ webhook_config = {
             'color': 0xFB3512
         }
     },
+    'dota2': {
+        'name': 'Dota 2',
+        'tag': ('dota2', 'dota'),
+        'emoji': '🎮',
+        'class': URLWebhook,
+        'kwargs': {
+            'source': 'https://store.steampowered.com/news/?appids=570&appgroupname=Dota+2&feed=steam_updates',
+            'navigate_html': lambda soup: soup.find(class_='newsPostBlock steam_updates').find('a').get('href'),
+            'poll_delay': 60 * 60 * 24,  # 1 day
+            'footer': ('Dota2',
+                       'https://steamcdn-a.akamaihd.net/steam/apps/570/capsule_184x69.jpg'),
+            'color': 0xFB3512
+        }
+    },
     'league_of_legends': {
         'name': 'League of Legends',
         'tag': ('league of legends', 'league', 'lol'),
@@ -59,7 +73,7 @@ logger = logging.getLogger(__name__)
 
 class Webhooks:
 
-    __slots__ = ('bot', 'darkest_dungeon', 'realm_royale') #'league_of_legends')
+    __slots__ = ('bot', 'darkest_dungeon', 'dota2', 'realm_royale') #'league_of_legends')
 
     def __init__(self, bot):
         self.bot = bot
