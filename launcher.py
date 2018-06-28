9b1743bbd94f24e3e30d7c7211ada1d47b047947
Geoffrey Houy <geoffhouy@gmail.com>
2018-06-27 23:31:52 -0400

    Move logging setup to its own function

--- launcher.py
+++ launcher.py
@@ -5,12 +5,16 @@ import logging
 import sys
 
 
-if __name__ == '__main__':
+def setup_logging():
     logging.getLogger('discord').setLevel(logging.WARNING)
     logging.basicConfig(
         format='%(asctime)s %(levelname)s %(name)s: %(message)s',
         datefmt="%Y-%m-%d %H:%M:%S",
         level=logging.INFO,
         stream=sys.stdout)
+
+
+if __name__ == '__main__':
+    setup_logging()
     bot = ZOINKS()
     bot.run(config.DISCORD_TOKEN, reconnect=True)
