b382ff0c4cc45ee13843a9996840829ab2f59603
Geoffrey Houy <geoffhouy@gmail.com>
2018-06-29 18:42:10 -0400

    Change initialization to just require a Steam app ID instead of a full URL

--- zoinks/webhooks/steam_webhook.py
+++ zoinks/webhooks/steam_webhook.py
@@ -1,3 +1,4 @@
+from zoinks.bot import ZOINKS
 from zoinks.webhooks.webhooks import ScrapingWebhook
 
 import discord
@@ -10,9 +11,36 @@ logger = logging.getLogger(__name__)
 
 
 class SteamWebhook(ScrapingWebhook):
+    """Represents a URL-scraping webhook that outputs to the specified Discord endpoint URL.
 
-    def __init__(self, bot, endpoint_url, **kwargs):
-        super().__init__(bot, endpoint_url, **kwargs)
+    Specifically used to scrape the specified Steam RSS feed then build and post a rich
+    discord.Embed message into the specified Discord channel.
+
+    Attributes
+    ----------
+    bot: ZOINKS
+        The currently running ZOINKS Discord bot. Used for its session attribute.
+    endpoint_url: str
+        The Discord webhook endpoint URL. Pass either the entire URL or all content after '/webhooks/'.
+    username: str
+        The Discord webhook username. Used to override the current webhook name.
+    avatar_url: str
+        The Discord webhook avatar image URL. Used to override the current webhook image.
+    steam_app_id: int
+        The Steam app's numerical ID found in its store URL.
+    delay: int
+        The time in seconds between checking for new content to post.
+    color: int
+        The color of the discord.Embed to post. Typically matched with the homepage color scheme.
+    thumbnail_url: str
+        The thumbnail URL of the discord.Embed to post. Typically used for logos.
+    """
+    def __init__(self, bot: ZOINKS, endpoint_url: str, steam_app_id: int=None, **kwargs):
+        if steam_app_id is None:
+            raise ValueError('Steam app ID must be set')
+        source_url = f'https://steamcommunity.com/games/{steam_app_id}/rss/'
+
+        super().__init__(bot=bot, endpoint_url=endpoint_url, source_url=source_url, **kwargs)
 
     async def find_url(self):
         soup = await self.fetch_soup(self.source_url)
