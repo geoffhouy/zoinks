232c7d9a28c105717705337183bda0ca9fbdf825
Geoffrey Houy <geoffhouy@gmail.com>
2018-07-13 12:08:59 -0400

    Fix extension loading

--- zoinks/bot.py
+++ zoinks/bot.py
@@ -9,16 +9,6 @@ import os
 command_prefix = '!'
 description = 'Like ZOINKS Scoob!'
 
-"""extensions = ('zoinks.cogs.berry',
-              'zoinks.cogs.new_member',
-              'zoinks.cogs.quote',
-              'zoinks.cogs.pin_popular',
-              'zoinks.cogs.twitch_notifier',
-              'zoinks.cogs.youtube_notifier',
-              #'zoinks.cogs.webhooks',
-              'zoinks.cogs.league_of_legends')
-              #'zoinks.cogs.realm_royale')"""
-
 logger = logging.getLogger(__name__)
 
 
@@ -29,12 +19,26 @@ class ZOINKS(commands.AutoShardedBot):
             command_prefix=command_prefix,
             description=description,
             pm_help=None)
+
         self.session = aiohttp.ClientSession(loop=self.loop)
 
-        extensions = set(
-            [os.path.splitext(module)[0] for module in os.listdir('zoinks/cogs/') if module.endswith('.py')])
+        extensions = set([
+            f'zoinks.cogs.{os.path.splitext(module)[0]}'
+            for module in os.listdir('zoinks/cogs/')
+            if module.endswith('.py') and 'init' not in module])
 
         self.load_extensions(extensions)
+
+    async def close(self):
+        await super().close()
+        await self.session.close()
+
+    async def on_ready(self):
+        await self.change_presence(activity=discord.Game(name=f'ZOINKS! | {self.command_prefix}help'))
+        logger.info(f'{self.user} logged in')
+
+    async def on_command_error(self, ctx, error):
+        pass
 
     def load_extensions(self, extensions: set=()):
         if len(extensions) > 0:
@@ -43,37 +47,3 @@ class ZOINKS(commands.AutoShardedBot):
                     self.load_extension(extension)
                 except ModuleNotFoundError as e:
                     logger.warning(e)
-
-    async def on_ready(self):
-        await self.change_presence(activity=discord.Game(name=f'ZOINKS! | {command_prefix}help'))
-        logger.info(f'{self.user} logged in')
-
-    async def on_message(self, message):
-        if message.author.bot:
-            return
-        await self.process_commands(message)
-
-    async def on_command_error(self, ctx, error):
-        if hasattr(ctx.command, 'on_error'):
-            return
-
-        error = getattr(error, 'original', error)
-
-        do_nothing = (commands.errors.CommandNotFound, commands.errors.DisabledCommand)
-        if isinstance(error, do_nothing):
-            return
-
-        warn = (commands.errors.BadArgument, commands.errors.MissingRequiredArgument,
-                commands.errors.TooManyArguments, commands.errors.UserInputError)
-        if isinstance(error, warn):
-            await ctx.send(embed=discord.Embed(
-                title='⚠ Error',
-                description=f'{str(error)}\n\nUse `{command_prefix}help {ctx.command.name}` for more information.',
-                color=0xFFCB00))
-            return
-
-        logger.info(f'{ctx.command.name} command exception ignored: {error}')
-
-    async def close(self):
-        await super().close()
-        await self.session.close()
