52a72885756b2df3c20683fca36da2ebf6df2e70
Geoffrey Houy <geoffhouy@gmail.com>
2018-07-14 18:22:54 -0400

    Add MadLibs command

--- zoinks/cogs/random.py
+++ zoinks/cogs/random.py
@@ -5,6 +5,7 @@ from discord.ext import commands
 
 import asyncio
 import logging
+import re
 import random
 
 
@@ -12,8 +13,11 @@ logger = logging.getLogger(__name__)
 
 ZOINKS_EMOJI = '<:ZOINKS:463062621134782474>'
 
-with open('zoinks/resources/text/quotes.txt', 'r') as quotes_file:
-    quotes = [line.rstrip().replace('|', '\n') for line in quotes_file]
+with open('zoinks/resources/text/madlibs.txt', 'r', encoding='utf8') as file:
+    madlibs = [line.rstrip() for line in file]
+
+with open('zoinks/resources/text/quotes.txt', 'r', encoding='utf8') as file:
+    quotes = [line.rstrip().replace('|', '\n') for line in file]
 
 responses = (
     'It is certain.', 'It is decidedly so.', 'Without a doubt.', 'Yes - definitely.', 'You may rely on it.',
@@ -27,6 +31,45 @@ class Random:
 
     def __init__(self, bot):
         self.bot = bot
+
+    @commands.command(aliases=['madlib'])
+    async def madlibs(self, ctx):
+        """Starts a game of MadLibs."""
+        timeout = 30
+        regex = re.compile(r'`(.+?)`+?')
+
+        embed = discord.Embed(title='📓 MadLibs', color=zoinks.bot.color)
+
+        embed.description = f'The game will begin shortly.\n\nYou have {timeout} seconds to respond with each word.'
+        await ctx.send(embed=embed)
+        await asyncio.sleep(3)
+
+        madlib = random.choice(madlibs)
+        blanks = regex.findall(madlib)
+        answers = []
+
+        def check(message):
+            return message.channel == ctx.channel and not message.author.bot
+
+        for blank in blanks:
+            grammar = 'n' if blank.lower().startswith(('a', 'e', 'i', 'o', 'u')) else ''
+            embed.description = f'I need a{grammar} `{blank}`.'
+            await ctx.send(embed=embed, delete_after=timeout)
+
+            try:
+                answer = await ctx.bot.wait_for('message', check=check, timeout=timeout)
+            except asyncio.TimeoutError:
+                embed.description = 'Nobody responded in time.\n\nThis game has been cancelled.'
+                return await ctx.send(embed=embed)
+            else:
+                answers.append(answer.content)
+
+            await asyncio.sleep(1)
+
+        madlib = regex.sub(lambda replace: str(answers.pop(0)), madlib)
+
+        embed.description = f'The MadLib is complete!\n\n{madlib}'
+        await ctx.send(embed=embed)
 
     @commands.command(aliases=['8ball'])
     async def magic8ball(self, ctx):
@@ -54,7 +97,9 @@ class Random:
 
     @commands.command()
     async def scramble(self, ctx):
-        """Allows the user to play a word scramble with the bot."""
+        """Starts a scrambled word puzzle."""
+        timeout = 30
+
         embed = discord.Embed(title='📚 Scrambled Word Puzzle', color=zoinks.bot.color)
 
         async with self.bot.session.post(url='http://watchout4snakes.com/wo4snakes/Random/RandomWord') as response:
@@ -66,14 +111,15 @@ class Random:
 
         scrambled_word = ''.join(random.sample(word, len(word)))
 
-        embed.description = f'The scrambled word is `{scrambled_word}`.\n\nYou have 30 seconds to unscramble the word.'
+        embed.description = (f'The scrambled word is `{scrambled_word}`.\n\n' 
+                             f'You have {timeout} seconds to respond with the unscrambled word.')
         await ctx.send(embed=embed)
 
         def check(message):
             return message.content == word and message.channel == ctx.channel
 
         try:
-            answer = await ctx.bot.wait_for('message', check=check, timeout=30)
+            answer = await ctx.bot.wait_for('message', check=check, timeout=timeout)
         except asyncio.TimeoutError:
             embed.description = f'Nobody solved the puzzle.\n\nThe word was `{word}`.'
             await ctx.send(embed=embed)
