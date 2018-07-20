9f56cdebeb2b54d19a450f7bd36ae7fdbc748121
Geoffrey Houy <geoffhouy@gmail.com>
2018-07-20 19:39:41 -0400

    Update _request to have a region parameter

--- zoinks/riot_games_api.py
+++ zoinks/riot_games_api.py
@@ -81,29 +81,25 @@ class RiotGamesAPI:
 
     def __init__(self, bot):
         self._api_key = config.RIOT_GAMES_API_KEY
-        self._region = 'na'
+        self._default_region = 'na'
         self.bot = bot
 
-    @property
-    def region(self):
-        return self._region
-
-    @region.setter
-    def region(self, region: str):
-        if region not in REGION:
-            raise ValueError(f'Invalid region: {region}')
-        self._region = region
-
-    async def _request(self, url: str, **kwargs):
+    async def _request(self, url: str, region: str, **kwargs):
         params = {'api_key': self._api_key}
         for key, value in kwargs.items():
             if key not in params:
                 params[key] = value
 
-        url = URL['base'].format(platform=REGION[self.region]['platform'], url=url)
+        if region is None:
+            region = self._default_region
+        elif region not in REGION:
+            raise ValueError(f'Invalid region: {region}')
+
+        url = URL['base'].format(platform=REGION[region]['platform'], url=url)
 
         async with self.bot.session.get(url=url, params=params) as response:
             return await response.json() if response.status == 200 else None
 
-    async def get_summoner_by_name(self, name):
-        return await self._request(url=URL['summoner_by_name'].format(version=VERSION['summoner'], name=name))
+    async def get_summoner_by_name(self, name: str, region: str=None):
+        return await self._request(url=URL['summoner_by_name'].format(version=VERSION['summoner'], name=name),
+                                   region=region)
