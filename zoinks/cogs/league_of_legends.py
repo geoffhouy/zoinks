33ca7a0a71b5cce368cd90f1a3ac018ecbf549ff
Geoffrey Houy <geoffhouy@gmail.com>
2018-07-22 14:09:45 -0400

    Add regions command

--- zoinks/cogs/league_of_legends.py
+++ zoinks/cogs/league_of_legends.py
@@ -1,3 +1,9 @@
+import zoinks.bot
+from zoinks.riot_games_api import RiotGamesAPI, REGION
+
+import discord
+from discord.ext import commands
+
 import logging
 
 
@@ -8,9 +14,17 @@ class LeagueOfLegends:
 
     def __init__(self, bot):
         self.bot = bot
+        self.api = RiotGamesAPI(self.bot)
         logger.info(f'{self.__class__.__name__} loaded')
 
-    # TODO: Riot Games API Integration
+    @commands.command()
+    async def regions(self, ctx):
+        embed = discord.Embed(title='🌎 League of Legends Regions',
+                              description='Below is a list of available regions for League of Legends commands.',
+                              color=zoinks.bot.color)
+        embed.add_field(name='Region Name', value='\n'.join([REGION[key]['name'] for key in REGION.keys()]))
+        embed.add_field(name='Usage in Commands', value='\n'.join(REGION.keys()))
+        await ctx.send(embed=embed)
 
 
 def setup(bot):
